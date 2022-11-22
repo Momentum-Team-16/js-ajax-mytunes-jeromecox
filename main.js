83e00a74ee23f7a82db9d76cf3156373443418a6
jeromecox <jeromecox@hotmail.com>
2022-11-21 23:03:55 -0600

    working on fetch

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 const page = document.querySelector("#container");
-const searchInput = document.querySelector("search-bar");
+const searchInput = document.querySelector("#search-bar");
 const searchResults = document.querySelector("#search-results");
-
-function getItunesData() {
-  let url = "https://itunes.apple.com/search?" + searchInput.value;
-
-  fetch(url)
-    .then((data) => data.json())
-    .then((json) => {
-      console.log(json);
-    })
-
-    .catch((error) => console.log(error));
-}
 
 page.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  // let search;
+  console.log(`Search: ${searchInput.value}`);
+
+  function getItunesData() {
+    let url = "https://itunes.apple.com/search?" + searchInput.value;
+
+    fetch(url)
+      .then((data) => data.json())
+      .then((json) => {
+        console.log(json);
+      })
+
+      .catch((error) => console.log(error));
+  }
+
+  getItunesData();
+  //
 
   // let card = document.createElement("div");
   // card.classList.add("card");
@@ -30,21 +33,21 @@ page.addEventListener("submit", function (event) {
   // let thumb = document.createElement("div");
   // thumb.classList.add("card-image");
   // let pic = document.createElement("img");
-  // // pic.src = albumArt;
+  // pic.src = ${song.artworkUrl100};
 
   // thumb.appendChild(pic);
   // card.appendChild(thumb);
 
   // let track = document.createElement("div");
   // track.classList.add("card-content");
-  // // let trackTitle = importObject.trackName;
+  // let trackTitle = ${song.trackName};
 
   // card.appendChild(track);
 
   // let name = document.createElement("div");
   // name.classList.add("card-title");
-  // let artName = importObject.results.artistName;
-  // // name.innerText = artName;
+  // let artName = ${song.artistName};
+  // name.innerText = artName;
 
   // card.appendChild(name);
 
